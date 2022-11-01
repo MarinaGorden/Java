ffe847775be5ac76f181d272d83b19a6b752e06b
Yingchun Duan <74944077+MarinaGorden@users.noreply.github.com>
2022-11-01 10:42:48 +0000

    Add SelectionSort.java with test into the basic sorting, rewrite the InsertionSort.java

--- src/Sorting/BasicSorting/InsertionSort/InsertionSort.java
+++ src/Sorting/BasicSorting/InsertionSort/InsertionSort.java
@@ -13,19 +13,17 @@ public class InsertionSort {
         // Average: Time O(N^2) | Space O(1)
         // Worst: Time O(N^2) | Space O(1)
         int len = array.length;
-        for (int i = 0; i < len - 1; i++) {
-            // pick the minimum element index at [i+1, len-1)
-            int minIndex = i;
-            for (int j = i + 1; j <len; j++) {
-                if(array[j] < array[minIndex]) {
-                    minIndex = j;
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j-1] > array[j]) {
+                    swap(array, j-1, j);
+                } else {
+                    break;
                 }
             }
-            swap(array, i, minIndex);
         }
         return array;
     }
-
     static void swap(int[] array, int i, int j) {
         int tmp = array[i];
         array[i] = array[j];
