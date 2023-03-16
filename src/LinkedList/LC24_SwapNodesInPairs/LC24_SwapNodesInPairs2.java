08813dae1dc494669f58fc2b9935bb0391461ead
Yingchun Duan <74944077+MarinaGorden@users.noreply.github.com>
2023-03-16 08:28:19 +0000

    fast slow node with iteration solution on LC 24

--- src/LinkedList/LC24_SwapNodesInPairs/LC24_SwapNodesInPairs2.java
+++ src/LinkedList/LC24_SwapNodesInPairs/LC24_SwapNodesInPairs2.java
@@ -1,4 +1,23 @@
 package LinkedList.LC24_SwapNodesInPairs;
 
+import LinkedList.ListNode;
+
 public class LC24_SwapNodesInPairs2 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+
+        while (head != null && head.next != null) {
+            ListNode slow = head, fast = head.next;
+            prev.next = fast;
+            slow.next = fast.next;
+            fast.next = slow;
+            // reinitialize the head and prev(tmp)
+            prev = slow;
+            // jump to the next half 3 -> 4
+            dummyHead = slow.next;
+        }
+        return dummyHead.next;
+    }
 }
